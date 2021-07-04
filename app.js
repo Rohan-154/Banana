b29fe5719c931875bc83aa1e9efc5e36adc59e2b
Rohan-154 <86555869+Rohan-154@users.noreply.github.com>
2021-07-04 14:21:16 +0530

    Input output done

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 var btnTranslate= document.querySelector("#btn-translate");
 var txtInput= document.querySelector("#txt-input"); 
-console.log(txtInput);
+var outputDiv= document.querySelector("#output");
+
+
 
 
  function clickHandler(){
-    console.log("clicked");
-    console.log("input", textInput.value); 
+  
+   outputDiv.innerText= "translated" + txtInput.value;
     
  };
  btnTranslate.addEventListener("click", clickHandler)
 
+// 1. document.querySelector("textarea")
+// 2. .btn-primary
+// 3. #input-btn
+// 4. input [name= "translator"
