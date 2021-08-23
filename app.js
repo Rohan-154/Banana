89846ef148a2b8a9ec564d0051eceb53cfb8442b
Rohan-154 <86555869+Rohan-154@users.noreply.github.com>
2021-08-23 22:14:35 +0530

    changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var btnTranslate = document.querySelector("#btn-translate"); //selecting button 
 var txtInput = document.querySelector("#txt-input"); // selecting input
 var outputDiv = document.querySelector("#output"); //outputDiv selected
 
-// var serverURL= "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
 
 var serverURL = "https://api.funtranslations.com/translate/minion.json";
 function getTranslationURl(text) {
